0297df0bcc2a779a0829927775731556e553aad1
cristopxer <cristopherortiz457@gmail.com>
2022-12-22 21:23:48 -0600

    Details Screen Completed

--- components/index.js
+++ components/index.js
@@ -2,5 +2,17 @@ import HomeHeader from "./HomeHeader";
 import FocusedStatusBar from "./FocusedStatusBar";
 import ArtCard from "./ArtCard";
 import { CircleButton, RectButton } from "./Button";
+import DetailsDesc from "./DetailsDesc";
+import DetailsBid from "./DetailsBid";
+import { SubInfo } from "./SubInfo";
 
-export { HomeHeader, FocusedStatusBar, ArtCard, CircleButton, RectButton };
+export {
+  HomeHeader,
+  FocusedStatusBar,
+  ArtCard,
+  CircleButton,
+  RectButton,
+  DetailsDesc,
+  DetailsBid,
+  SubInfo,
+};
