a836da063ca8f1b84e1284a263e389b291f24756
cristopxer <cristopherortiz457@gmail.com>
2022-12-27 21:37:55 -0600

    tailwind confg/home second second section completed

--- marketart/src/App.js
+++ marketart/src/App.js
@@ -1,5 +1,25 @@
+import { Download, Features, SectionWrapper } from "./components";
+import assets from "./assets";
+
 const App = () => {
-  return <div className="App">Project init</div>;
+  return (
+    <>
+      <SectionWrapper
+        title="Let's find a masterpiece"
+        description="Buy, store, collect art pieces from everyehere. Join 33+ million people using ArtMarketplace"
+        showBtn
+        mockupImg={assets.homeHero}
+        banner="banner"
+      />
+      <SectionWrapper
+        title="Smart user interface marketplace"
+        description="Experience and fluent interface with smooth and constant colors"
+        mockupImg={assets.homeCards}
+        reverse
+      />
+      <Features />
+    </>
+  );
 };
 
 export default App;
